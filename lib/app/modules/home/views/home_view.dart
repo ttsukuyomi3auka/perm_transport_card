0263f192884cf3eeb7e20b544cf31407d0505faa
tsukuyomi <tsumi@mail>
2024-04-11 00:21:36 +0500

    add background

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:perm_transport_card/resources/resources.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -9,16 +10,19 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('HomeView'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(Images.background),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child:
+              const Center(child: Image(image: AssetImage(Images.studentCard))),
+        ));
   }
 }
---
+++ lib/resources/images.dart
@@ -0,0 +1,9 @@
+part of 'resources.dart';
+
+class Images {
+  Images._();
+
+  static const String background = 'assets/images/background.jpg';
+  static const String defaultCard = 'assets/images/default_card.png';
+  static const String studentCard = 'assets/images/student_card.png';
+}
